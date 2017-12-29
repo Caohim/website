e1078ba96941d7dcf31c14403d1090646dd427d8
Caohim <64G7 88zi74Kk 44O>
2017-12-29 22:08:58 +0100

    Init
    Based on online version

---
+++ index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+
+  <head>
+    <title>Caohim | Développeur Front-end</title>
+    <meta charset="UTF-8">
+
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <meta property="og:title" content="Caohim | Développeur Front-end" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://caohim.com" />
+    <!-- TO DO: Social img -->
+    <meta property="og:image" content="./img/social.jpg" />
+    <meta property="og:site_name" content="Caohim, développeur front-end" />
+    <meta property="og:locale:alternate" content="fr_BE" />
+
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="apple-touch-icon" sizes="57x57" href="./favicons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="./favicons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="./favicons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="./favicons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="./favicons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="./favicons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="./favicons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="./favicons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="./favicons/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="./favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="./favicons/favicon-194x194.png" sizes="194x194">
+    <link rel="icon" type="image/png" href="./favicons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="./favicons/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="./favicons/favicon-16x16.png" sizes="16x16">
+    <!--[if IE]>
+      <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+    <![endif]-->
+    <link rel="manifest" href="./favicons/manifest.json">
+    <meta name="msapplication-TileColor" content="#2b5797">
+    <meta name="msapplication-TileImage" content="./favicons/mstile-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <meta name="HandheldFriendly" content="True">
+    <link rel="stylesheet" media="all" type="text/css" href="./css/global.css" />
+
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-23250995-1']);
+      _gaq.push(['_setDomainName', '.caohim.com']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
+
+  </head>
+
+  <body>
+    <div class="wrapper">
+
+      <header class="site-header u-text-center" role="heading">
+        <div class="u-row">
+          <!-- Tea or coffee? -->
+          <h1 class="author">Caohim</h1>
+          <h2 class="author-title">Développeur front-end</h2>
+          <ul class="social">
+            <li class="social-item"><a href="mailto:info@caohim.com" class="social-link" title="Écrivez-moi&nbsp;!"><i class="i-letter"></i><span class="u-placeholder">Email</span></a></li>
+            <li class="social-item"><a href="//twitter.com/caohim" class="social-link" title="Mon compte Twitter" target="_blank"><i class="i-twitter"></i><span class="u-placeholder">Twitter</span></a></li>
+            <li class="social-item"><a href="//codepen.io/caohim" class="social-link" title="Mes expérimentations sur Codepen" target="_blank"><i class="i-codepen"></i><span class="u-placeholder">Codepen</span></a></li>
+            <li class="social-item"><a href="//linkedin.com/in/kevin-d-60a49646/" class="social-link" title="Mon profil sur LinkedIn" target="_blank"><i class="i-linkedin"></i><span class="u-placeholder">LinkedIn</span></a></li>
+          </ul>
+          <div class="page-tools"><a href="./en.php" title="See page in English">EN</a> // FR</div>
+        </div>
+      </header>
+
+      <main class="site-main" role="main">
+        <section class="about">
+          <h2 class="u-outline">À propos</h2>
+          <div class="u-row u-clearfix">
+            <div class="u-half u-left u-padding-r">
+              <h3><i class="i-marker"></i> En ce moment</h3>
+              <p class="intro">Vis actuellement à Liège (Belgique) et travaille en tant que freelance comme développeur front-end. C’est ainsi que l’on nomme le maillon entre un designer et un développeur, ayant un pied dans les deux matières à la fois ; yup&nbsp;! Plus génériquement, je suis un concepteur de sites web.</p>
+              <p>Sinon, vous me trouverez devant mon ordinateur à trifouiller et expérimenter, ou simplement à l'extérieur, sous un arbre, à sourire aux couleurs de l’automne en forêt&nbsp;!</p>
+            </div>
+            <div class="u-half u-left u-padding-l">
+              <h3><i class="i-men"></i> En bref</h3>
+              <p class="intro">Observateur, avec une soif d’apprendre chaque jour, je n’hésite jamais à m’engager entièrement dans ce que je fais, en mettant l’accent sur la qualité et les détails face à la quantité.</p>
+              <p>À côté de cela, je prends plaisir à contempler la nature, écouter et vivre la musique (particulièrement pagan, folk et métal), marcher dans les forêts wallonnes, m’extasier devant les paysages scandinaves, déguster des crêpes bretonnes et de l’hydromel. Un petit mélange de tout cela, entre autres&nbsp;!</p>
+            </div>
+          </div>
+        </section>
+        <section class="clients theme--blue">
+          <h2 class="u-outline">Projets</h2>
+          <div class="u-row u-row--followed">
+            <h3><i class="i-star"></i> Clients</h3>
+            <ul class="clients-list">
+              <li>OTAN (NATO)</li>
+              <li>Ice-Watch</li>
+              <li>EVS</li>
+              <li>RubyMotion</li>
+              <li>European University Association</li>
+              <li>Global Crop Diversity Trust</li>
+              <li>KitoZyme</li>
+              <li>Groupe Eloy</li>
+              <li>Quatuor</li>
+              <li>Jean-Philippe Darcis</li>
+              <li>BCO Aviation</li>
+              <li>…et bien d’autres&nbsp;!</li>
+            </ul>
+          </div>
+          <div class="u-row">
+            <h3><i class="i-trophy"></i> Reconnaissances</h3>
+            <ul class="recognitions-list">
+              <li class="recognition">
+                <h4 class="recognition-project">Site Of The Day (Awwwards)</h4>
+                <ul>
+                  <li><a href="http://www.awwwards.com/web-design-awards/crop-trust-annual-report-2014" target="_blank">Crop Trust - Annual Report 2014</a> (chez <a href="http://epic.net" target="_blank">EPIC Agency</a>)</li>
+                  <li><a href="http://www.awwwards.com/web-design-awards/babel-the-king" target="_blank">Babel the cat who would be King</a> (chez <a href="http://epic.net" target="_blank">EPIC Agency</a>)</li>
+                  <li><a href="http://www.awwwards.com/web-design-awards/ice-phone" target="_blank">Ice-phone</a> (chez <a href="http://epic.net" target="_blank">EPIC Agency</a>)</li>
+                </ul>
+              </li>
+              <li class="recognition">
+                <h4 class="recognition-project">Honorable Mention (Awwwards)</h4>
+                <ul>
+                  <li><a href="http://www.awwwards.com/best-websites/leidgens-piscines/" target="_blank">Leidgens Piscines</a> (chez <a href="http://epic.net" target="_blank">EPIC Agency</a>)</li>
+                  <li><a href="http://www.awwwards.com/best-websites/my-winebox/" target="_blank">My Winebox</a> (chez <a href="http://epic.net" target="_blank">EPIC Agency</a>)</li>
+                </ul>
+              </li>
+              <li class="recognition">
+                <h4 class="recognition-project">Site Of The Day (FWA)</h4>
+                <ul>
+                  <li><a href="http://www.thefwa.com/site/the-ice-phone" target="_blank">Ice-phone</a> (chez <a href="http://epic.net" target="_blank">EPIC Agency</a>)</li>
+                </ul>
+              </li>
+              <li class="recognition">
+                <h4 class="recognition-project">Site Of The Day (CSSNectar)</h4>
+                <ul>
+                  <li><a href="http://www.csswinner.com/details/crop-trust-2014-annual-report/9727" target="_blank">Crop Trust - Annual Report 2014</a> (chez <a href="http://epic.net" target="_blank">EPIC Agency</a>)</li>
+                </ul>
+              </li>
+              <li class="recognition">
+                <h4 class="recognition-project">Website Of The Day (CSSWinner)</h4>
+                <ul>
+                  <li><a href="http://www.csswinner.com/details/crop-trust-2014-annual-report/9727" target="_blank">Crop Trust - Annual Report 2014</a> (chez <a href="http://epic.net" target="_blank">EPIC Agency</a>)</li>
+                </ul>
+              </li>
+              <li class="recognition">
+                <h4 class="recognition-project">Site Of The Day (BestCSS)</h4>
+                <ul>
+                  <li><a href="http://www.bestcss.in/user/detail/CropTrust2014AnnualReport-3099" target="_blank">Crop Trust - Annual Report 2014</a> (chez <a href="http://epic.net" target="_blank">EPIC Agency</a>)</li>
+                </ul>
+              </li>
+              <li class="recognition">
+                <h4 class="recognition-project">Website Of The Day (CSSDesignAwards)</h4>
+                <ul>
+                  <li><a href="http://www.cssdesignawards.com/sites/crop-trust-annual-report-2014/26957/" target="_blank">Crop Trust - Annual Report 2014</a> (chez <a href="http://epic.net" target="_blank">EPIC Agency</a>)</li>
+                </ul>
+              </li>
+              <li class="recognition">
+                <h4 class="recognition-project">Site Of The Day (CSSAWDS)</h4>
+                <ul>
+                  <li><a href="http://www.cssawds.com/website/crop-trust-2014-annual-report/" target="_blank">Crop Trust - Annual Report 2014</a> (chez <a href="http://epic.net" target="_blank">EPIC Agency</a>)</li>
+                </ul>
+              </li>
+              <li class="recognition">
+                <h4 class="recognition-project">Site Du Jour (French Design Index)</h4>
+                <ul>
+                  <li><a href="http://www.frenchdesignindex.com/design-index-10226" target="_blank">Crop Trust - Annual Report 2014</a> (chez <a href="http://epic.net" target="_blank">EPIC Agency</a>)</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </main>
+
+      <footer class="site-footer u-text-center">
+        <div class="u-row">
+          <blockquote>Je suis convaincu qu’il existe un juste milieu entre une certaine admiration pour la nature et une passion pour les technologies.</blockquote>
+        </div>
+      </footer>
+
+    </div>
+  </body>
+</html>
